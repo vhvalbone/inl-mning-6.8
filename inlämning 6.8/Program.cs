f0605e7e72fe3e2f3cc861ebde84a7cd3a926a58
vhvalbone <albin06svensson@gmail.com>
2024-02-28 10:22:10 +0100

    Add project files.

---
+++ inlämning 6.8/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Ange ett heltal:");
+        int heltal = Convert.ToInt32(Console.ReadLine());
+
+        int antalPrimtal = AntalPrimtalMindreAn(heltal);
+        Console.WriteLine("Antalet primtal mindre än " + heltal + " är: " + antalPrimtal);
+    }
+
+    public static bool ArPrimtal(int n)
+    {
+        if (n <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static int AntalPrimtalMindreAn(int n)
+    {
+        int antalPrimtal = 0;
+        for (int i = 2; i < n; i++)
+        {
+            if (ArPrimtal(i))
+            {
+                antalPrimtal++;
+            }
+        }
+        return antalPrimtal;
+    }
+}
+
